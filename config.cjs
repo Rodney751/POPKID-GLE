50fab85ebdb640984145549c7d2265a12f19e5b1
Rodney751 <rodneyraphael18@gmail.com>
2025-07-09 19:00:00 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const config = {
   // 🧠 Session & Identity
-  SESSION_ID: process.env.SESSION_ID || "Popkidmd$MLJjtNih",
+  SESSION_ID: process.env.SESSION_ID || "Shadow-Xtech~tmdCXZKZ#8GgFgIpJsK9a2oLD13e58thCT1gcd1F-h4O_xyGfd-Q",
   PREFIX: process.env.PREFIX || '.',
   BOT_NAME: process.env.BOT_NAME || "POPKID GLE",
   BOT: process.env.BOT || "hello 👋",
